6a7f1ccdc3026691ae65be835d9be10d39e9c18d
MechanicalBill <william.richards@gmail.com>
2016-12-14 22:48:00 -0600

    Impelemented the AGNES algorithm. After running the program, 2 files get created as output. First is the raw data of the flights and their clustering level, and the second is the clustering diagram in csv form.

---
+++ AGNES/FlightDataPoint.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AGNES
+{
+    class FlightDataPoint
+    {
+        public FlightDataPoint()
+        {
+            ClusteringLevel = 0;
+        }
+        public int FlightId { get; set; }
+        public double PointToPointDistance { get; set; }
+        public double Price { get; set; }
+        public double ClusteringLevel { get; set; }
+
+        public double EuclideanDistanceTo(FlightDataPoint p)
+        {
+            return Math.Sqrt(Math.Pow(p.PointToPointDistance - this.PointToPointDistance, 2) + Math.Pow(p.Price - this.Price, 2));
+        }
+    }
+}
